3f4df3610261d60b483ad654344ecd786736a2f6
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-06-26 13:46:37 +0000

    when docs are wrong, only the logs can save you...

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.sendgrid.net",
   port: "587",
   authentication: :plain,
-  user_name: ENV["SENDGRID_USERNAME"],
+  user_name: ENV["SENDGRID_USERNAME"], # CAREFUL! should be user_name instead of username!
   password: ENV["SENDGRID_PASSWORD"],
   domain: "heroku.com",
   enable_starttls_auto: true
