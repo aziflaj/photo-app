d9c2f9b56466d808738528e16322a7ebfbc89dc4
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-06-26 15:15:04 +0000

    show form controls for Stripe

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
+gem 'stripe'
 
 group :development, :test do
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626121049) do
+ActiveRecord::Schema.define(version: 20160626145139) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string   "email"
+    t.string   "token"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
