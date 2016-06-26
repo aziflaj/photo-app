7f5f7365b36e31dc483f1f805eae4709f8f83178
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-06-26 13:40:45 +0000

    hotfix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 class WelcomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :hello]
 
   def index
+  end
+
+  def hello
+    render json: {
+      username: ENV["SENDGRID_USERNAME"],
+      password: ENV["SENDGRID_PASSWORD"]
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+  get '/hello' => 'welcome#hello'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
